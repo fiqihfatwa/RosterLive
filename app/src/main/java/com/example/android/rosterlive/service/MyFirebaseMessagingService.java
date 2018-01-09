311e7865cd503015eab1c3a6e5082177d9d9485b
isadadi <idadi70@gmail.com>
2018-01-10 00:14:47 +0700

    notif

---
+++ app/src/main/java/com/example/android/rosterlive/service/MyFirebaseMessagingService.java
@@ -0,0 +1,44 @@
+package com.example.android.rosterlive.service;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+
+import com.example.android.rosterlive.MainActivity;
+import com.example.android.rosterlive.R;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by isadadi on 10/01/2018.
+ */
+
+public class MyFirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        showNotification(remoteMessage.getData().get("message"));
+    }
+
+    private void showNotification(String message){
+        Intent i = new Intent(this, MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+
+        android.support.v4.app.NotificationCompat.Builder builder = new android.support.v4.app.NotificationCompat.Builder(this)
+                .setAutoCancel(true)
+                .setContentTitle("RosterLive")
+                .setContentText(message)
+            //   .setSmallIcon(android.support.design.R.drawable.)
+                .setSound(alarmSound)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(0,builder.build());
+    }
+}
